5b35978b53efd17613174c4b9b479f90dc9a95fc
Pie98 <pielotti1998@gmail.com>
2023-11-28 15:07:22 +0100

    modified:   Data_visualization.py 	modified:   exploratory_data_analysis.ipynb

--- Data_visualization.py
+++ Data_visualization.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import random
 import pandas as pd
+import numpy as np
 
 def visualize_home_win_season(dataframe, season = 'Random', team='Random'):
     df_win_loss = dataframe[['stagione','hometeam','awayteam','home_result']].reset_index(drop=True)
@@ -64,3 +65,125 @@ def visualize_home_win_season(dataframe, season = 'Random', team='Random'):
 
     plt.show()
     return 0
+
+
+def visualize_gol_wins_last_5(dataframe):
+
+  fig, axs = plt.subplots(2, 2, figsize=(24, 14))
+
+  for i in range(2):
+    for j in range(2):
+      # calcoliamo i percentili di tiri per ogni partita
+      stagione = random.choice(dataframe['stagione'].unique())
+      #prendo le righe dalla 50 in poi perché nelle prime 5 giornate non ci sono i 5 giorni precedenti valorizzati
+      df_anno = dataframe[dataframe['stagione']==stagione][['home_last_5_days_shots_target','away_last_5_days_shots_target','home_result']].iloc[49:]
+      media_tiri_ultime_5 = list(df_anno['home_last_5_days_shots_target'])+ list(df_anno['away_last_5_days_shots_target'])
+
+      #percentile_tiri = np.percentile(media_tiri_ultime_5)
+      percentile_tiri = np.round(np.median(media_tiri_ultime_5),0)
+
+      #creo due nuove colonne con i percentili 
+      conditions_1 = [
+      (df_anno['home_last_5_days_shots_target'] <= percentile_tiri),  
+      #( (percentile_tiri[0] <= df_anno['home_last_5_days_shots_target']) & (df_anno['home_last_5_days_shots_target'] < percentile_tiri[1] )), 
+      #( (percentile_tiri[1] <= df_anno['home_last_5_days_shots_target']) & (df_anno['home_last_5_days_shots_target'] <= percentile_tiri[2] )), 
+      (df_anno['home_last_5_days_shots_target'] > percentile_tiri)
+      ]
+
+      conditions_2 = [
+      (df_anno['away_last_5_days_shots_target'] <= percentile_tiri),  
+      #( (percentile_tiri[0] <= df_anno['away_last_5_days_shots_target']) & (df_anno['away_last_5_days_shots_target'] < percentile_tiri[1] )), 
+      #( (percentile_tiri[1] <= df_anno['away_last_5_days_shots_target']) & (df_anno['away_last_5_days_shots_target'] <= percentile_tiri[2] )), 
+      (df_anno['away_last_5_days_shots_target'] > percentile_tiri)
+      ]
+
+      # Valori corrispondenti alle condizioni
+      values = [f'<={percentile_tiri}',
+                #f'fra {percentile_tiri[1]} e {percentile_tiri[0]}', f'fra {percentile_tiri[1]} e {percentile_tiri[2]}',
+                  f'> {percentile_tiri}']
+      # Creazione della nuova colonna 
+      df_anno['home_last_5_days_shot_count'] = np.select(conditions_1, values)
+      df_anno['away_last_5_days_shot_count'] = np.select(conditions_2, values)
+
+
+      df_anno_home = df_anno[['home_last_5_days_shot_count','home_result']].groupby('home_last_5_days_shot_count')['home_result'].value_counts().unstack()
+      df_anno_away = df_anno[['away_last_5_days_shot_count','home_result']].groupby('away_last_5_days_shot_count')['home_result'].value_counts().unstack()
+
+      df_tiri_vittorie = df_anno_home[['L','D','W']]
+      df_tiri_vittorie['W'] = df_anno_home['W'] + df_anno_away['L']
+      df_tiri_vittorie['L'] = df_anno_home['L'] + df_anno_away['W']
+      df_tiri_vittorie['D'] = df_anno_home['D'] + df_anno_away['D']
+
+      ax = axs[i, j]
+      # Disegna il grafico nel subplot corrente
+      ax = df_tiri_vittorie.plot(kind="bar", figsize=(10, 7), color=['red','lightblue','green'],ax=ax, title=f'stagione={stagione}')
+      ax.legend(bbox_to_anchor=(1.0, 1.0))
+      fig.subplots_adjust(hspace=0.4,wspace=0.4)   
+      # Impostare i label dell'asse x non ruotati
+      ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
+      # Assegnare un nome all'asse x
+      ax.set_xlabel("shots last 5 matches")
+      
+
+  plt.show()
+
+
+def visualize_wins__yellows_last_5(dataframe):
+
+  fig, axs = plt.subplots(2, 2, figsize=(24, 14))
+
+  for i in range(2):
+    for j in range(2):
+      # calcoliamo i percentili di tiri per ogni partita
+      stagione = random.choice(dataframe['stagione'].unique())
+      #prendo le righe dalla 50 in poi perché nelle prime 5 giornate non ci sono i 5 giorni precedenti valorizzati
+      df_anno = dataframe[dataframe['stagione']==stagione][['home_last_5_days_yellows','away_last_5_days_yellows','home_result']].iloc[49:]
+      media_tiri_ultime_5 = list(df_anno['home_last_5_days_yellows'])+ list(df_anno['away_last_5_days_yellows'])
+
+      #percentile_tiri = np.percentile(media_tiri_ultime_5)
+      percentile_tiri = np.round(np.median(media_tiri_ultime_5),0)
+
+      #creo due nuove colonne con i percentili 
+      conditions_1 = [
+      (df_anno['home_last_5_days_yellows'] <= percentile_tiri),  
+      #( (percentile_tiri[0] <= df_anno['home_last_5_days_yellows']) & (df_anno['home_last_5_days_yellows'] < percentile_tiri[1] )), 
+      #( (percentile_tiri[1] <= df_anno['home_last_5_days_yellows']) & (df_anno['home_last_5_days_yellows'] <= percentile_tiri[2] )), 
+      (df_anno['home_last_5_days_yellows'] > percentile_tiri)
+      ]
+
+      conditions_2 = [
+      (df_anno['away_last_5_days_yellows'] <= percentile_tiri),  
+      #( (percentile_tiri[0] <= df_anno['away_last_5_days_yellows']) & (df_anno['away_last_5_days_yellows'] < percentile_tiri[1] )), 
+      #( (percentile_tiri[1] <= df_anno['away_last_5_days_yellows']) & (df_anno['away_last_5_days_yellows'] <= percentile_tiri[2] )), 
+      (df_anno['away_last_5_days_yellows'] > percentile_tiri)
+      ]
+
+      # Valori corrispondenti alle condizioni
+      values = [f'<={percentile_tiri}',
+                #f'fra {percentile_tiri[1]} e {percentile_tiri[0]}', f'fra {percentile_tiri[1]} e {percentile_tiri[2]}',
+                  f'> {percentile_tiri}']
+      # Creazione della nuova colonna 
+      df_anno['home_last_5_days_yellow_count'] = np.select(conditions_1, values)
+      df_anno['away_last_5_days_yellows'] = np.select(conditions_2, values)
+
+
+      df_anno_home = df_anno[['home_last_5_days_yellow_count','home_result']].groupby('home_last_5_days_yellow_count')['home_result'].value_counts().unstack()
+      df_anno_away = df_anno[['away_last_5_days_yellows','home_result']].groupby('away_last_5_days_yellows')['home_result'].value_counts().unstack()
+
+      df_tiri_vittorie = df_anno_home[['L','D','W']]
+      df_tiri_vittorie['W'] = df_anno_home['W'] + df_anno_away['L']
+      df_tiri_vittorie['L'] = df_anno_home['L'] + df_anno_away['W']
+      df_tiri_vittorie['D'] = df_anno_home['D'] + df_anno_away['D']
+
+      ax = axs[i, j]
+      # Disegna il grafico nel subplot corrente
+      ax = df_tiri_vittorie.plot(kind="bar", figsize=(10, 7), color=['red','lightblue','green'],ax=ax, title=f'stagione={stagione}')
+      ax.legend(bbox_to_anchor=(1.0, 1.0))
+      fig.subplots_adjust(hspace=0.4,wspace=0.4)   
+      # Impostare i label dell'asse x non ruotati
+      ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
+      # Assegnare un nome all'asse x
+      ax.set_xlabel("shots last 5 matches")
+      
+
+  plt.show()
