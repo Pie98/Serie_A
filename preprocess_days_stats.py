5dd19b941fcb735fb749b31735a56838467c16fd
Pie98 <pielotti1998@gmail.com>
2023-11-29 14:44:49 +0100

    new file:   Deep_learning_win_predict.ipynb 	modified:   __pycache__/preprocess_days_stats.cpython-311.pyc 	new file:   __pycache__/preprocess_features.cpython-311.pyc 	renamed:    Win_predict_stats.ipynb -> notebooks/Win_predict_stats.ipynb 	modified:   preprocess_days_stats.py 	new file:   preprocess_features.py

--- preprocess_days_stats.py
+++ preprocess_days_stats.py
@@ -4,6 +4,78 @@ import pandasql as ps
 import os
     
 
+def preprocess_match_days(directory_path):
+    concatenated_df = pd.DataFrame({})
+    #leggo il path di ogni csv e lo converto in dataframe
+    for file_name in os.listdir(fr"{directory_path}"):
+        if file_name.endswith('.csv'):
+            print(f'Reading file: {file_name}')
+            file_path = os.path.join(directory_path, file_name)
+
+        temp_df = pd.read_csv(file_path ,parse_dates=['Date'], dayfirst=True )
+        temp_df['Date'] = temp_df['Date'].dt.strftime('%d/%m/%Y')
+        
+        # Concatenare i DataFrame
+        concatenated_df = pd.concat([concatenated_df,temp_df], ignore_index=True)
+
+    #prendo solo le colonne importanti al fine della statistica e ordino per data il df
+    concatenated_df_important = concatenated_df[['Div', 'Date', 'HomeTeam', 'AwayTeam', 'FTHG','FTAG', 'FTR', 'HTHG', 'HTAG', 'HTR',
+                                                'HS', 'AS', 'HST', 'AST', 'HF', 'AF', 'HC',
+                                                 'AC', 'HY', 'AY', 'HR', 'AR','B365H','B365D','B365A']]
+    
+    concatenated_df_important = concatenated_df_important.dropna(subset=['HomeTeam']) #se manca la quadra di casa la statistica è inutile
+    concatenated_df_important['Date'] = pd.to_datetime(concatenated_df_important['Date'])
+    df_importanti = concatenated_df_important.sort_values(by='Date', ascending=False)    
+
+    query_giornate = f''' 
+                    SELECT 
+                    `Div` AS div,
+                    CEIL(CAST((ROW_NUMBER() OVER (PARTITION BY stagione ORDER BY Date)) AS float)/10) AS giornata,
+                    stagione AS stagione,
+                    Date AS date,
+                    HomeTeam AS hometeam,
+                    AwayTeam as awayteam,
+                    FTHG AS ft_home_goals,
+                    FTAG AS ft_away_goals,
+                    FTR AS ft_result,
+                    HTHG ht_home_goals,
+                    HTAG ht_away_goals,
+                    HTR ht_results,
+                    HS home_shots,
+                    `AS` AS away_shots,
+                    HST AS home_shots_targ,
+                    AST AS away_shots_targ,
+                    HF AS home_fouls,
+                    AF AS away_fouls,
+                    HC home_corners,
+                    AC away_corners,
+                    HY home_yellow,
+                    AY away_yellow,
+                    HR home_red,
+                    AR away_red,
+                    B365H home_win_odds,
+                    B365D draw_odds,
+                    B365A away_win_odds
+                    FROM (
+                        SELECT 
+                            *,
+                            CASE
+                                WHEN strftime('%m', Date) < '08' THEN strftime('%Y', Date, '-1 year') || '/' || strftime('%Y', Date)
+                                WHEN strftime('%m', Date) = '08' AND strftime('%d', Date) < '10' THEN strftime('%Y', Date, '-1 year') || '/' || strftime('%Y', Date)
+                                ELSE strftime('%Y', Date) || '/' || strftime('%Y', Date, '+1 year')
+                            END AS Stagione,
+                            Date    
+                        FROM df_importanti PSA )
+                    ORDER BY date, hometeam    
+    '''
+
+    df_giornate = ps.sqldf(query_giornate, locals())
+    df_giornate['date'] = pd.to_datetime(df_giornate['date'])
+    df_giornate['giornata'] = df_giornate['giornata'].astype(int)
+    print('preprocessing finished!')
+    
+    return df_giornate      
+
 def preprocess_cumulative_stats(dataframe = [], directory = [], giorni_cumulativi = 5):
 
     if (len(directory) != 0):
@@ -263,77 +335,6 @@ def preprocess_cumulative_stats(dataframe = [], directory = [], giorni_cumulativ
     df_Serie_A['date'] = pd.to_datetime(df_Serie_A['date'])
     print('preprocess finished')
     return df_Serie_A, Statistiche_squadre_dict
-    
-def preprocess_match_days(directory_path):
-    concatenated_df = pd.DataFrame({})
-    #leggo il path di ogni csv e lo converto in dataframe
-    for file_name in os.listdir(fr"{directory_path}"):
-        if file_name.endswith('.csv'):
-            print(f'Reading file: {file_name}')
-            file_path = os.path.join(directory_path, file_name)
-
-        temp_df = pd.read_csv(file_path ,parse_dates=['Date'], dayfirst=True )
-        temp_df['Date'] = temp_df['Date'].dt.strftime('%d/%m/%Y')
-        
-        # Concatenare i DataFrame
-        concatenated_df = pd.concat([concatenated_df,temp_df], ignore_index=True)
-
-    #prendo solo le colonne importanti al fine della statistica e ordino per data il df
-    concatenated_df_important = concatenated_df[['Div', 'Date', 'HomeTeam', 'AwayTeam', 'FTHG','FTAG', 'FTR', 'HTHG', 'HTAG', 'HTR',
-                                                'HS', 'AS', 'HST', 'AST', 'HF', 'AF', 'HC',
-                                                 'AC', 'HY', 'AY', 'HR', 'AR','B365H','B365D','B365A']]
-    
-    concatenated_df_important = concatenated_df_important.dropna(subset=['HomeTeam']) #se manca la quadra di casa la statistica è inutile
-    concatenated_df_important['Date'] = pd.to_datetime(concatenated_df_important['Date'])
-    df_importanti = concatenated_df_important.sort_values(by='Date', ascending=False)    
-
-    query_giornate = f''' 
-                    SELECT 
-                    `Div` AS div,
-                    CEIL(CAST((ROW_NUMBER() OVER (PARTITION BY stagione ORDER BY Date)) AS float)/10) AS giornata,
-                    stagione AS stagione,
-                    Date AS date,
-                    HomeTeam AS hometeam,
-                    AwayTeam as awayteam,
-                    FTHG AS ft_home_goals,
-                    FTAG AS ft_away_goals,
-                    FTR AS ft_result,
-                    HTHG ht_home_goals,
-                    HTAG ht_away_goals,
-                    HTR ht_results,
-                    HS home_shots,
-                    `AS` AS away_shots,
-                    HST AS home_shots_targ,
-                    AST AS away_shots_targ,
-                    HF AS home_fouls,
-                    AF AS away_fouls,
-                    HC home_corners,
-                    AC away_corners,
-                    HY home_yellow,
-                    AY away_yellow,
-                    HR home_red,
-                    AR away_red,
-                    B365H home_win_odds,
-                    B365D draw_odds,
-                    B365A away_win_odds
-                    FROM (
-                        SELECT 
-                            *,
-                            CASE
-                                WHEN strftime('%m', Date) < '08' THEN strftime('%Y', Date, '-1 year') || '/' || strftime('%Y', Date)
-                                WHEN strftime('%m', Date) = '08' AND strftime('%d', Date) < '10' THEN strftime('%Y', Date, '-1 year') || '/' || strftime('%Y', Date)
-                                ELSE strftime('%Y', Date) || '/' || strftime('%Y', Date, '+1 year')
-                            END AS Stagione,
-                            Date    
-                        FROM df_importanti PSA )
-                    ORDER BY date, hometeam    
-    '''
-
-    df_giornate = ps.sqldf(query_giornate, locals())
-    df_giornate['date'] = pd.to_datetime(df_giornate['date'])
-    df_giornate['giornata'] = df_giornate['giornata'].astype(int)
-    print('preprocessing finished!')
-    
-    return df_giornate   
+     
     
     
---
+++ preprocess_features.py
@@ -0,0 +1,110 @@
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+import numpy as np 
+import pandas as pd 
+
+def preprocess_columns(dataframe,numero_colonne,giorni_cumulativi):
+    all_columns = ['giornata', 'stagione','hometeam', 'awayteam','home_total_points','home_result', 'away_total_points', f'home_last_{giorni_cumulativi}_days_ft_goals',
+               f'away_last_{giorni_cumulativi}_days_ft_goals',f'home_last_{giorni_cumulativi}_days_ht_goals',f'away_last_{giorni_cumulativi}_days_ht_goals', f'away_last_{giorni_cumulativi}_days_shots',
+       f'home_last_{giorni_cumulativi}_days_ft_goals_conceded',f'away_last_{giorni_cumulativi}_days_ft_goals_conceded',
+       f'home_last_{giorni_cumulativi}_days_shots',f'home_last_{giorni_cumulativi}_days_shots_target',
+       f'away_last_{giorni_cumulativi}_days_shots_target',f'home_last_{giorni_cumulativi}_days_fouls_done',f'away_last_{giorni_cumulativi}_days_fouls_done',f'home_last_{giorni_cumulativi}_days_corners_obtained',
+       f'away_last_{giorni_cumulativi}_days_corners_obtained',f'home_last_{giorni_cumulativi}_days_yellows',f'away_last_{giorni_cumulativi}_days_yellows',
+       f'home_last_{giorni_cumulativi}_days_reds', f'away_last_{giorni_cumulativi}_days_reds']
+
+    less_columns = ['stagione','hometeam', 'awayteam','home_total_points','home_result', 'away_total_points',f'home_last_{giorni_cumulativi}_days_ft_goals',
+       f'away_last_{giorni_cumulativi}_days_ft_goals',f'home_last_{giorni_cumulativi}_days_ft_goals_conceded',f'away_last_{giorni_cumulativi}_days_ft_goals_conceded',f'home_last_{giorni_cumulativi}_days_shots',
+       f'away_last_{giorni_cumulativi}_days_shots',f'home_last_{giorni_cumulativi}_days_yellows',f'away_last_{giorni_cumulativi}_days_yellows']
+
+    few_columns = ['hometeam', 'awayteam','home_total_points','home_result', 'away_total_points',f'home_last_{giorni_cumulativi}_days_ft_goals',
+               f'away_last_{giorni_cumulativi}_days_ft_goals',f'home_last_{giorni_cumulativi}_days_ft_goals_conceded',
+               f'away_last_{giorni_cumulativi}_days_ft_goals_conceded',f'home_last_{giorni_cumulativi}_days_shots',
+               f'away_last_{giorni_cumulativi}_days_shots']
+
+
+    if numero_colonne == 'all':
+       colonne = all_columns
+       print('utilizzando tutte le features')
+    elif numero_colonne == 'less':
+       print('utilizzando meno features')
+       colonne = less_columns
+    else:
+       print('utilizzando poche features')
+       colonne=few_columns
+
+
+    for day in range(giorni_cumulativi):
+       colonne.append(f'home_result_{day+1}')
+       colonne.append(f'away_result_{day+1}')
+
+    df_stats_serie_A = dataframe[colonne]   
+
+    #creo il train valid test
+    X_train_df, X_test_df, Train_labels, Test_labels = train_test_split(df_stats_serie_A.drop(f'home_result', axis=1), df_stats_serie_A[f'home_result'],
+                                                                     test_size=0.1, random_state=42)
+
+    X_train_df, X_valid_df, Train_labels, Valid_labels = train_test_split(X_train_df, Train_labels, test_size=0.055, random_state=42)
+
+    print(f'lunghezza dataframe train {len(X_train_df)}, \n lunghezza dataframe valid {len(X_valid_df)}, \n lunghezza dataframe test {len(X_test_df)}')
+
+    all_numerical_columns= [f'home_total_points',f'away_total_points',f'home_last_{giorni_cumulativi}_days_ft_goals',f'away_last_{giorni_cumulativi}_days_ft_goals',f'home_last_{giorni_cumulativi}_days_ht_goals',
+       f'away_last_{giorni_cumulativi}_days_ht_goals', f'home_last_{giorni_cumulativi}_days_ft_goals_conceded',f'away_last_{giorni_cumulativi}_days_ft_goals_conceded',
+       f'home_last_{giorni_cumulativi}_days_shots',f'home_last_{giorni_cumulativi}_days_shots_target',f'away_last_{giorni_cumulativi}_days_shots',f'away_last_{giorni_cumulativi}_days_shots_target',
+       f'home_last_{giorni_cumulativi}_days_fouls_done',f'away_last_{giorni_cumulativi}_days_fouls_done',f'home_last_{giorni_cumulativi}_days_corners_obtained',
+       f'away_last_{giorni_cumulativi}_days_corners_obtained',f'home_last_{giorni_cumulativi}_days_yellows',f'away_last_{giorni_cumulativi}_days_yellows',f'home_last_{giorni_cumulativi}_days_reds', 
+       f'away_last_{giorni_cumulativi}_days_reds']
+
+    less_numerical_columns = [f'home_total_points',f'away_total_points',f'home_last_{giorni_cumulativi}_days_ft_goals',f'away_last_{giorni_cumulativi}_days_ft_goals',f'home_last_{giorni_cumulativi}_days_ft_goals_conceded',
+                            f'away_last_{giorni_cumulativi}_days_ft_goals_conceded',f'away_last_{giorni_cumulativi}_days_shots',f'home_last_{giorni_cumulativi}_days_shots',
+                            f'home_last_{giorni_cumulativi}_days_yellows',f'away_last_{giorni_cumulativi}_days_yellows']
+
+    few_numerical_columns = [f'home_total_points',f'away_total_points',f'home_last_{giorni_cumulativi}_days_ft_goals',f'away_last_{giorni_cumulativi}_days_ft_goals',f'home_last_{giorni_cumulativi}_days_ft_goals_conceded',
+                            f'away_last_{giorni_cumulativi}_days_ft_goals_conceded',f'away_last_{giorni_cumulativi}_days_shots',f'home_last_{giorni_cumulativi}_days_shots']
+
+
+    all_categorical_columns = ['giornata', 'stagione',f'hometeam', f'awayteam']
+    less_categorical_columns = ['stagione',f'hometeam', f'awayteam']
+    few_categorical_columns = [f'hometeam', f'awayteam']
+
+    if numero_colonne == 'all':
+        categorical_columns = all_categorical_columns
+        numerical_columns = all_numerical_columns
+    elif numero_colonne == 'less':
+        categorical_columns = less_categorical_columns
+        numerical_columns = less_numerical_columns
+    else:
+        categorical_columns=few_categorical_columns
+        numerical_columns = few_numerical_columns
+
+
+    for day in range(giorni_cumulativi):
+        categorical_columns.append(f'home_result_{day+1}')
+        categorical_columns.append(f'away_result_{day+1}')
+
+    #Creo un column transformer, rende più facile normalizzare il dataframe
+    ct = make_column_transformer(
+    (MinMaxScaler(), numerical_columns), #normalizza queste colonne con minmax
+    (OneHotEncoder(handle_unknown='ignore'), categorical_columns) ,# le colonne da one hot
+        #encodare, tutte le altre le ignora grazie al comando handle_unknown
+    sparse_threshold=0  
+    )
+
+    ct.fit(X_train_df)
+
+    # trasformo train e test
+    X_train_norm = ct.transform(X_train_df)
+    X_valid_norm  = ct.transform(X_valid_df)
+    X_test_norm = ct.transform(X_test_df)
+
+    # Crea un OrdinalEncoder
+    ordinal_encoder = OrdinalEncoder()
+    # Addestra l'OrdinalEncoder su Train_labels e applica la trasformazione
+    Train_labels_encoded = ordinal_encoder.fit_transform(np.array(Train_labels).reshape(-1, 1))
+
+    Train_labels_encoded = np.squeeze(ordinal_encoder.transform(np.array(Train_labels).reshape(-1, 1)))
+    Valid_labels_encoded = np.squeeze(ordinal_encoder.transform(np.array(Valid_labels).reshape(-1, 1)))
+    Test_labels_encoded = np.squeeze(ordinal_encoder.transform(np.array(Test_labels).reshape(-1, 1)))  
+
+    return  X_train_norm, X_valid_norm, X_test_norm, Train_labels_encoded, Valid_labels_encoded, Test_labels_encoded
