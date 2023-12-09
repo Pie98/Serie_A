aed4113a91d9a3e1131b9a9a87007fe2e2fa3248
Pie98 <pielotti1998@gmail.com>
2023-12-09 14:02:13 +0100

    modified:   create_models_time_series.py

--- create_models_time_series.py
+++ create_models_time_series.py
@@ -398,11 +398,11 @@ def create_time_series_model_lstm(Train_teams_shape, feature_input_shape, first_
     return model_1_final
 
 
-#################################################
+##################################################
 
 # --------------- Conv1D  model ---------------- #
 
-#################################################
+##################################################
 
 def create_time_series_model_conv1d(Train_teams_shape, feature_input_shape,num_filters, kernel_size, 
                                    second_dropout,concat_dropout_1,concat_dropout_2, num_features ):
@@ -591,4 +591,213 @@ def create_time_series_model_conv1d(Train_teams_shape, feature_input_shape,num_f
             outputs=output_layer,
             name='model_1_dense_concat'
         )
+    return model_1_final
+
+
+##################################################
+
+# ---------------- odds  model ----------------- #
+
+##################################################
+
+
+def create_time_series_model_odds(Train_teams_shape, feature_input_shape,num_filters, kernel_size, 
+                                   second_dropout,concat_dropout_1, concat_dropout_2, num_features,
+                                    odds_filters, odds_kernel, odds_dropout ):
+
+    #Modello per i teams 
+    inputs = layers.Input(shape=(Train_teams_shape,), name='teams_input')
+    x = layers.Reshape((Train_teams_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = 8, kernel_size=3, activation='relu', padding="causal")(x)
+    x = layers.Flatten()(x) #layers.Flatten()(x)
+    outputs = layers.Dense(8)(x)
+    model_teams = tf.keras.Model(inputs,outputs, name = 'model_1_teams')
+
+    # modello ft_goals
+    inputs = layers.Input(shape=(feature_input_shape,), name='goals_input')
+    x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)  
+    x = layers.Flatten()(x) #layers.Flatten()(x)
+    x = layers.Dense(16, activation='relu')(x)
+    outputs = layers.Dropout(second_dropout)(x) 
+    model_ft_goals = tf.keras.Model(inputs, outputs, name='model_1_goals')
+
+    # modello ft_goals_conceded
+    inputs = layers.Input(shape=(feature_input_shape,), name='goals_conceded_input')
+    x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)  
+    x = layers.Flatten()(x)                
+    x = layers.Dense(16, activation='relu')(x)
+    outputs = layers.Dropout(second_dropout)(x) 
+    model_ft_goals_conceded = tf.keras.Model(inputs, outputs, name='ft_goals_conceded')
+
+    # modello shots
+    inputs = layers.Input(shape=(feature_input_shape,), name='shotss_input')
+    x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)       
+    x = layers.Flatten()(x)          
+    x = layers.Dense(16, activation='relu')(x)
+    outputs = layers.Dropout(second_dropout)(x) 
+    model_shots = tf.keras.Model(inputs, outputs, name='model_1_shots')
+
+    # modello corners_obtained
+    inputs = layers.Input(shape=(feature_input_shape,), name='corners_input')
+    x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)                 
+    x = layers.Flatten()(x)
+    x = layers.Dense(16, activation='relu')(x)
+    outputs = layers.Dropout(second_dropout)(x) 
+    model_corners_obtained = tf.keras.Model(inputs, outputs, name='model_1_corners_obtained')
+
+    # se considero tutte le features inserisco anche gli shots on target e corner
+    if num_features == 'all':
+        # modello shots_target
+        inputs = layers.Input(shape=(feature_input_shape,), name='shots_target_input')
+        x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+        x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)           
+        x = layers.Flatten()(x)       
+        x = layers.Dense(16, activation='relu')(x)
+        outputs = layers.Dropout(second_dropout)(x) 
+        model_shots_target = tf.keras.Model(inputs, outputs, name='model_1_shots_target')
+
+        #Unisco i modelli dei tiri 
+        model_1_shots_concat_layer = layers.Concatenate(name="shots_concat")([model_shots.output, model_shots_target.output,
+                                                                               model_corners_obtained.output])
+        x = layers.Dense(64, activation='relu')(model_1_shots_concat_layer)
+        x = layers.Dropout(concat_dropout_1)(x)  # Aggiunto il layer di dropout per ridurre overfitting
+        output_layer_shots_concat = layers.Dense(32, activation='relu')(x)
+
+        #creo il modello  finale dei tiri
+        model_1_shots_concat =tf.keras.Model(
+            inputs=[[ model_shots.input, model_shots_target.input, model_corners_obtained.input]],
+            outputs=output_layer_shots_concat,
+            name='model_1_shots_concat'
+    )
+    
+    elif num_features=='less':
+        #Unisco i modelli dei tiri 
+        model_1_shots_concat_layer = layers.Concatenate(name="shots_concat")([model_shots.output,
+                                                                               model_corners_obtained.output])
+        x = layers.Dense(64, activation='relu')(model_1_shots_concat_layer)
+        x = layers.Dropout(concat_dropout_1)(x)  # Aggiunto il layer di dropout per ridurre overfitting
+        output_layer_shots_concat = layers.Dense(32, activation='relu')(x)
+
+        #creo il modello  finale dei tiri
+        model_1_shots_concat =tf.keras.Model(
+            inputs=[[ model_shots.input, model_corners_obtained.input]],
+            outputs=output_layer_shots_concat,
+            name='model_1_shots_concat'
+    )
+
+    # modello fouls_done
+    inputs = layers.Input(shape=(feature_input_shape,), name='fouls_input')
+    x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)   
+    x = layers.Flatten()(x)              
+    x = layers.Dense(16, activation='relu')(x)
+    outputs = layers.Dropout(second_dropout)(x) 
+    model_fouls_done = tf.keras.Model(inputs, outputs, name='model_1_fouls_done')
+
+    if num_features == 'all':
+        # modello yellows
+        inputs = layers.Input(shape=(feature_input_shape,), name='yellows_input')
+        x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+        x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)     
+        x = layers.Flatten()(x)             
+        x = layers.Dense(16, activation='relu')(x)
+        outputs = layers.Dropout(second_dropout)(x) 
+        model_yellows = tf.keras.Model(inputs, outputs, name='model_1_corners_yellows')
+
+    # modello reds
+    inputs = layers.Input(shape=(feature_input_shape,), name='reds_input')
+    x = layers.Reshape((feature_input_shape, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = num_filters, kernel_size=kernel_size, activation='relu', padding="causal")(x)    
+    x = layers.Flatten()(x)             
+    x = layers.Dense(16, activation='relu')(x)
+    outputs = layers.Dropout(second_dropout)(x) 
+    model_reds = tf.keras.Model(inputs, outputs, name='model_1_corners_reds')
+
+    if num_features == 'all':
+            #Unisco i modelli dei falli 
+        model_1_fouls_concat_layer = layers.Concatenate(name="fouls_concat")([model_fouls_done.output, model_yellows.output, model_reds.output])
+        x = layers.Dense(64, activation='relu')(model_1_fouls_concat_layer)
+        x = layers.Dropout(concat_dropout_1)(x)  # Aggiunto il layer di dropout per ridurre overfitting
+        output_layer_fouls_concat = layers.Dense(32, activation='relu')(x)
+
+        #creo il modello  finale dei falli
+        model_1_fouls_concat =tf.keras.Model(
+            inputs=[[ model_fouls_done.input, model_yellows.input, model_reds.input]],
+            outputs=output_layer_fouls_concat,
+            name='model_1_shots_concat' 
+        )
+
+    elif num_features == 'less':
+            #Unisco i modelli dei falli 
+        model_1_fouls_concat_layer = layers.Concatenate(name="fouls_concat")([model_fouls_done.output, model_reds.output])
+        x = layers.Dense(64, activation='relu')(model_1_fouls_concat_layer)
+        x = layers.Dropout(concat_dropout_1)(x)  # Aggiunto il layer di dropout per ridurre overfitting
+        output_layer_fouls_concat = layers.Dense(32, activation='relu')(x)
+
+        #creo il modello  finale dei falli
+        model_1_fouls_concat =tf.keras.Model(
+            inputs=[[ model_fouls_done.input, model_reds.input]],
+            outputs=output_layer_fouls_concat,
+            name='model_1_shots_concat' 
+        )
+
+    # Creo il modello delle odds
+    inputs = layers.Input(shape=(3,), name='odds_input')
+    x = layers.Reshape((3, 1))(inputs) # add an extra dimension for timesteps
+    x = layers.Conv1D(filters = odds_filters, kernel_size= odds_kernel, activation='relu', padding="causal")(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(10)(x)
+    outputs = layers.Dropout(odds_dropout)(x)
+    model_odds = tf.keras.Model(inputs,outputs, name = 'model_1_odds')
+    
+
+    if ((num_features == 'all') | (num_features=='less')):
+        #Unisco i modelli 
+        model_1_concat_layer = layers.Concatenate(name="feature_concat")([ model_teams.output, model_ft_goals.output, model_ft_goals_conceded.output, 
+                                                                model_1_shots_concat.output, model_1_fouls_concat.output, model_odds.output])
+        x = layers.Dense(64, activation='relu')(model_1_concat_layer)
+        x = layers.Dropout(concat_dropout_1)(x)
+        x = layers.Dense(32, activation='relu')(x)
+        x = layers.Dropout(concat_dropout_2)(x)
+        output_layer = layers.Dense(3, activation = 'softmax')(x)
+
+    else:
+        #Unisco i modelli 
+        model_1_concat_layer = layers.Concatenate(name="feature_concat")([ model_teams.output, model_ft_goals.output, model_ft_goals_conceded.output, 
+                                                                model_shots.output, model_reds.output, model_odds.output])
+        x = layers.Dense(64, activation='relu')(model_1_concat_layer)
+        x = layers.Dropout(concat_dropout_1)(x)
+        x = layers.Dense(32, activation='relu')(x)
+        x = layers.Dropout(concat_dropout_2)(x)
+        output_layer = layers.Dense(3, activation = 'softmax')(x)
+
+
+    #creo il modello  finale
+    if num_features == 'all':
+        model_1_final =tf.keras.Model(
+            inputs=[[model_teams.input, model_ft_goals.input, model_ft_goals_conceded.input, model_shots.input, model_shots_target.input, model_fouls_done.input, 
+                        model_corners_obtained.input, model_yellows.input, model_reds.input, model_odds.input]],
+            outputs=output_layer,
+            name='model_1_dense_concat'
+        )
+
+    if num_features == 'less':
+        model_1_final =tf.keras.Model(
+            inputs=[[model_teams.input, model_ft_goals.input, model_ft_goals_conceded.input, model_shots.input, model_fouls_done.input, 
+                        model_corners_obtained.input, model_reds.input, model_odds.input]],
+            outputs=output_layer,
+            name='model_1_dense_concat'
+        )
+    
+    if num_features == 'few':
+        model_1_final =tf.keras.Model(
+            inputs=[[model_teams.input, model_ft_goals.input, model_ft_goals_conceded.input, model_shots.input, model_reds.input, model_odds.input]],
+            outputs=output_layer,
+            name='model_1_dense_concat'
+        )
+    
     return model_1_final
